79cc473ea00217c2e0ea11f005b918d540e638e4
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-08 08:38:20 -0700

    First code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * using the puts fuction
+ * not using printf
+ * in order to display strings
+ */
+int main(void)
+{
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return (0);
+}
