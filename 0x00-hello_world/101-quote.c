be8f4148999ee696a505569cddf3c2ae82f32d45
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-08 12:27:16 -0700

    Program with errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,6 +14,6 @@ int main(void)
 	 * syntax for unix standard
 	 * write(output-standard, string,size_of_string)
 	 */
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
