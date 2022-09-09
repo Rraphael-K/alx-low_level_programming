95af97c0b05bd0928b3d6b2fb6200724b2d84395
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-09 06:41:25 -0700

    All the numbers in the world

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* declare the number placeholder */
+	int num = 0;
+
+	/* loop to print numbers in order */
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	} putchar('\n');
+	return (0);
+}
+
