ed29c4416f85040098c007d7227109dcbd7771e6
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-09 03:46:10 -0700

    Displaying the last of a random number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Varifying it a random number is
+ * positive or negative using the if statement
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	/* display the last digit of a random number */
+	int l_digit = n % 10;
+
+	if (l_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
+	} else if (l_digit < 6 && l_digit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
+	} else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
+	}
+	return (0);
+}
+
