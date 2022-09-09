ad1a18bf6dd29217c2f807c400725ab9127eb42c
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-09 07:32:54 -0700

    Number combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* declare number placeholder */
+	int num;
+
+	/*generate all posible combinations of the number 1 till 9 */
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	/* Ensure that the numbers make posible combinations */
+		if (num != '9')
+		{
+			/* print a comma and a space before the next combo */
+			putchar(',');
+			putchar(' ');
+		}	
+	 } /* create anew line */
+	putchar('\n');
+	return (0);
+}
