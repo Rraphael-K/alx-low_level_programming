0ea8f5450cba67cff11b515e9def71d859708c83
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-13 05:45:45 -0700

    unistd.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(0, "_putchar\n", 10);
+	return (0);
+}
