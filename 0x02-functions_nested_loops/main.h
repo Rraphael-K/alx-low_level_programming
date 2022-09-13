5d4f523ba1e43eff0729200c384fb336070a48c0
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-13 09:43:09 -0700

    External Function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,11 +5,13 @@
 #include <stdio.h>
 char ch, *string = "_putchar\n";
 
-void *print_alphabet(void)
+void *print_alphabet()
 {
 	char count;
 
 	for (count = 'a'; count <= 'z'; count++)
 		putchar(count);
 	putchar('\n');
+
+	return (0);
 }
