bbd0c48cbf57008952ec714d37a04f8fa66321ef
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-13 07:25:02 -0700

    External Lib

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	display("_putchar\n");
-	return (1);
+	/*
+	 * since Strings have a length
+	 * initial the length so we may loop through it
+	 * this will allow us to display the characters as a string
+	 * and not as an array
+	 */
+	int count, length_string = strlen(string);
+
+	/*loop through until the string is displayed */
+	for (count = 0; count <= length_string; count++)
+		putchar(string(length_string));
+
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,2 @@
-#include <unistd.h>
-/**
- * _put char - writes the character c to the stdout
- * @c: the character to print
- *
- * Return: Always 1 (success)
- * On error, -1 is returned, and errno is set appropriate;y.
- */
-int disply(char c[])
-{
-	return(write(1, &c, 10));
-}
+/* declaring the string for use */
+char *string = "_putchar";
