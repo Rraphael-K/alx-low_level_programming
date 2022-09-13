ecb2c1cfe0a5581c1236916bbb588b455dfbbabd
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-13 16:03:00 -0700

    External Lib

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - See description
+ *
+ * Description: Checks for lowercase characters
+ *
+ * Return: 1 if is lowercase esle return 2 if is uppercase
+ */
+int _islower(int c)
+{
+	char chr;
+	int low = 0;
+
+	/*check if the letters are lowercase*/
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{ 
+		/* display 1 if the ltetter is lowercase */
+		if (low == ch)
+			return (1);
+	} 
+	return (0);
+
+}
