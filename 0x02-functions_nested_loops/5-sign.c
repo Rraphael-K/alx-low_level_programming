7572009d2eed4cce23dbdf0daa14b01f5c41188b
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-14 01:44:40 -0700

    External Lib

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - Display the sign of the number
+ *
+ * Return: 1 if greater than 0, 0 if 0 else -1
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		return (1);
+	} else if (n = 0)
+	{
+		return (0);
+	} else
+	{
+		return (-1);
+	}
+}
