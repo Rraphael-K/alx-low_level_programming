1054db511ed7c581590debfd919d4b887d7e3c51
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-14 02:20:37 -0700

    Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - use of the abs() function
+ * @r: an integer from main file
+ *
+ * Description: display the valueof an integer
+ * Return: Value of the absolute value r
+ */
+int _abs(int)
+{
+	if (r >= 0)
+		return (r);
+	else
+		/* multiply by -1 to return a negetive value */
+		return (r * -1)
+}
