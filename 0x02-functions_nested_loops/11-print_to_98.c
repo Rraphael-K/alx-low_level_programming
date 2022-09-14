0ee7143c352fd76ee3c97aa7d6deee8301e47586
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-14 08:10:50 -0700

    Print out all numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,18 +17,20 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
+
 			printf(", ");
 		}
 		printf("\n");
 	}
 	else
 	{
-		for(; n >= 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 			
 			if (n == 98)
 				continue;
+			
 			printf(", ");
 		}
 		printf("\n");
