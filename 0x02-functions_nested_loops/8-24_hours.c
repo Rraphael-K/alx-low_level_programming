f3eb0c59b46cf7bc8650e73a21e05b6060825866
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-14 05:46:29 -0700

    All in a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - Hours of the day
+ *
+ * Return: 
+ */
+void jack_bauer(void)
+{
+	/* declare hours and minutes for the watch */
+	int hrs, min;
+
+	/* loop to create the digital watch from 00:00 am */
+	for (min = 0; min <= 59; min++)
+	{
+		for (hrs = min + 1; hrs <= 24; hrs++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+
+			/* end the loop */
+			if (min == 59 && hrs == 24)
+				continue;
+		}
+	}
+	_putchar('\n');
+}
