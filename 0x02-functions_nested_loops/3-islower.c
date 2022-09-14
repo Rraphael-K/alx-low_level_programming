23ad53a6e13e95b571886ca53d37f92edad3aede
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-13 17:31:14 -0700

    External Lib

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -16,7 +16,9 @@ int _islower(int c)
 	{
 		/* display 1 if the ltetter is lowercase */
 		/* compare with input form main function */
-		if (low != c)
+		if (low == c)
 		       low = 1;
+		else 
+			low = 0;
 	}return (low);
 }
