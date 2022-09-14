c18082ad3a80e9381d3dc37ad516cee9d802b86c
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-14 07:06:24 -0700

    Sum of Input

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Function for this lib
+ * @num: user input
+ * @num2: user input
+ *
+ * Return: Sum of num and num2
+ */
+int add(int num, int num2)
+{
+	int n;
+	/* add user inout */
+	n = num + num2;
+
+	return (n);
+
+}
