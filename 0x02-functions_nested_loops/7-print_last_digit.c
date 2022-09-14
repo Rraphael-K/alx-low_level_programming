73953a3a00814e9b1c0ae8b1f3822ea9cd62f5c7
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-14 02:41:46 -0700

    Last Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Display the lastdigit of a number
+ * @r: Int from user input from main file
+ *
+ * Return: r from input
+ */
+int print_last_digit(int r)
+{
+	_putchar(r % 10);
+}
