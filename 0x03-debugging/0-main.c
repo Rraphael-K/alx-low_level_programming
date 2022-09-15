8c70cae95591c58cd46da9d6521e4fd7f08773b2
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-15 06:18:05 -0700

    Testing function lib

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests fucntion that prints if integer is posotive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(98);
+
+	return (0);
+}
