16cd163eb099512906e30c0d102f4edd23fa99f8
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-15 08:14:59 -0700

    Largest number, script correction

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 mnumbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+in largest_number(int a, int b, int c)
+{
+	int largestt;
+
+	if (a >= b && a >= c)
+	{
+		largestt = a;
+	}
+	else if (b >= a && b >= c )
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
