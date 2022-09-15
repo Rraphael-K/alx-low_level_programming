41be6c7d197b0fb404fa84760c5dee19b280adbc
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-15 16:26:09 -0700

    Not 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints numbers till 9 but not 2 and 4
+ *
+ * Return 0;
+ */
+
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= 9)
+	{
+		if (a != '2' && a != '4')
+			_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
