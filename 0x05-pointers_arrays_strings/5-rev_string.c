5032e4497cbe172b07bfa6bb3b641c6ceb0c59eb
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-19 16:24:24 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+include <stdio.h>
+/**
+ * rev_string - Prints a string in revese
+ * follwed by a new line
+ * @s: Input string
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int length = 0, count = 0;
+	char swapA;
+
+	while (s[length] != '\0')
+		length++;
+
+	while (count < length--)
+	{
+		swapA = s[count];
+		s[count++] = s[length];
+		s[length = swapA;
+	}
+	putchar('\n');
+}
