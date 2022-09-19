9e43ec3e429b280fe598768ba71ee57cf3999215
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-19 15:18:44 -0700

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to _98 - Reset a value to a defaul one
  * @n: Default variable to be used to hold the value 98
- *
+ * Description: Resets value to 98
  * Return: Void - Nothing
  */
 
