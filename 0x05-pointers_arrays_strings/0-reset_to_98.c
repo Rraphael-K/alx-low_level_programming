6b205ebec0773d3db43e16c0485da567ad31d230
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-19 08:57:29 -0700

    Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to _98 - Reset a value to a defaul one
+ * @n: Default variable to be used to hold the value 98
+ *
+ * Return: Void - Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
