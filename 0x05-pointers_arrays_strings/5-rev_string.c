7c09c05dd1dabbaa5975375f10534e90e85e5230
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-19 16:37:17 -0700

    Reverse String via swapping

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,16 +11,36 @@
 void rev_string(char *s)
 {
 	int length = 0, count = 0;
-	char swapA;
+	char swapA, swapB;
 
 	while (s[length] != '\0')
 		length++;
 
-	while (count < length--)
+	while (count < length)
 	{
 		swapA = s[count];
-		s[count++] = s[length];
-		s[length] = swapA;
+		swapB = s[length];
+		s[length++] = swapB;
+		s[count--] = swapA
 	}
 	putchar('\n');
+}
+
+/**
+ * _strlen - Returns a given string length
+ * @s: Checks the length of a string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count =0;
+
+	while (s[count] != '\0')
+		count++;
+
+	return (count);
 }
