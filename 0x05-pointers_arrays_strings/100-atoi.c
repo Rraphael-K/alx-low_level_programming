96e2407aa03614f2201eb871ba51c3ae4a238709
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-19 20:58:00 -0700

    Convert String to Int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * _atoi - Convert string to integer
+ * @s: String input
+ *
+ * Return: Integer from conversion
+ */
+
+int _atoi(char *s)
+{
+	int position = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			position *+ -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+		else if (null_flag)
+			break;
+		s++;
+	}
+
+	if (position < 0)
+		total = (-total);
+
+	return (total);
+}
