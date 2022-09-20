178b4a2f2321fd23b11032ca78215214385c26af
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-19 20:35:37 -0700

    Half the String

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half -  Prints half the string with a new line
+ * @str: String Input
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int length = 0, count, nHalf;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+		nHalf = length / 2;
+	else
+		nHalf = (length +1) / 2;
+
+	for (count = nHalf; count < length; count++)
+		putchar(str[count]);
+	putchar('\n');
+}
