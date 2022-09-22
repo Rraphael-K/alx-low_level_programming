3aff3abbffbd76f0fa85c07cfd05699dd99e6fd8
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-22 05:50:57 -0700

    Prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 char *_strcat(char *dest, char *src);
-/**
 char *_strncat(char *dest, char *src, int n);
 char *strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -15,5 +14,5 @@ void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-*/
+
 #endif /* MAIN_H */
