6bfa990ebc3d7690170ffeb2d49d1df57c6a7964
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-23 02:33:23 -0700

    Update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,7 @@ char *rot13(char *str)
 		while ((s[count] >= 'a' && s[count] <= 'z')
 			       	|| (s[count] >= 'A' && s[count] <= 'Z'))
 		{
-			if (s[count] >= 'a' && s[count] <= 'm)
+			if ((s[count] >= 'a' && s[count] <= 'm)
 				|| (s[count] >= 'A' && s[count] <= 'M'))
 					s[count] += 13;
 			else
