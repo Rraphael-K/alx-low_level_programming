fc26178a6a169594de037a5aa9d19dd4c3eb1e23
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-22 20:56:41 -0700

    Encode 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - Encode string to 1337
+ * @s: String Input
+ * Return: Coded string
+ */
+
+char *leet(char *string)
+{
+	int count = 0, counter;
+	char sub[10] = {'4', '4', '3', '3', '0', '0', '7', '7'. '1', '1'};
+	char str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (string[count])
+	{
+		for (counter =; counter < 10; counter++)
+			if (string(count) == str[counter])
+				s[count] = sub[counter];
+
+		count++;
+	}
+	return (string);
+}
