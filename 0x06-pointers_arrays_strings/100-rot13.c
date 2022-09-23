1637ff3bb7d7a80fafc358147033f7e543d6b6e5
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-23 02:28:58 -0700

    Updates

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - Encode string using rot13
+ * @str: Input string
+ * Return: String
+ */
+
+char *rot13(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		while ((s[count] >= 'a' && s[count] <= 'z')
+			       	|| (s[count] >= 'A' && s[count] <= 'Z'))
+		{
+			if (s[count] >= 'a' && s[count] <= 'm)
+				|| (s[count] >= 'A' && s[count] <= 'M'))
+					s[count] += 13;
+			else
+				s[count] -= 13;
+			count++;
+		}
+		count++
+	}
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * base10 - Power in Base 10
+ * @n: Exponent
+ * Return: Power Exponent
+ */
+
+int base10(int n)
+{
+	int base = 10;
+
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
+}
+/**
+ * print_number - print integers
+ * @n: Print integer
+ * Return: Void
+ */
+
+void print_number(int n)
+{
+	int p;
+	p = base10(8);
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		while (n / p == 0)
+			p /= 10;
+
+		while 9p >= 1)
+		{
+			_putchar((n / p) + '0');
+			n %= p;
+			p /= 10;
+		}
+	}
+}
