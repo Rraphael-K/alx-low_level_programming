d1af8047579704dd8fe09ab58d4223e54520a1fc
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-26 10:55:24 -0700

    Chessboard

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chessboard
+ * @a: array input
+ *
+ * return: Void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int index1, index2;
+
+	for (index1 = 0; index1 < 8; index1++)
+	{
+		for (index2 = 0; index2 < 8; index2++)
+			putchar(a[index1][index2]);
+		putchar('\n');
+	}
+}
