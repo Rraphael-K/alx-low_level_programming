416581dbb81051c964539599f88ab46328449415
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-28 03:02:23 -0700

    Exponential Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function returns valueof x to power y
+ * @x: Basse
+ * @y: Exponent
+ *
+ * Return: x to the power of y
+ */
+
+int _pw_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	return (x * _pw_recursion(x, y - 1));
+}
