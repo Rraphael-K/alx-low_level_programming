5ad7039ded75c9df5a7da459ee8ad91bfc7ec571
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-28 03:51:32 -0700

    Palidrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_palidrome - Checks if s is a palidrome string
+ * @s: String Input
+ *
+ * Return: 1 if string is palidrome or 0
+ */
+int is_palidrome(char *s)
+{
+	int len;
+
+	len = _strlen_recursion(s);
+	if (len <= 1)
+		return (1);
+
+	return (palid(s + 1, len - 2));
+}
+
+/**
+ * palid - Reverse string
+ * @s: String Input
+ * @len: Length of s
+ *
+ * Return: Reverse string
+ */
+int palid(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len -1))
+	{
+		return (palid(s + 1, len - 2));
+	}
+	else
+		return (0);
+}
