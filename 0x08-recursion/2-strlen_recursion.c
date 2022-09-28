e0b2b34f349e8a53600d04be179b2a742c7c4e9b
Rraphael-K <sifisoskhoza@gmail.com>
2022-09-28 02:31:52 -0700

    Length of String

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Return length of string
+ * @s: String Input
+ *
+ * Return: Length of String
+ */int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
