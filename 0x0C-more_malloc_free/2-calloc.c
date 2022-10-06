e47db40e29fede5ac61904ec7726743913756a60
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-06 14:15:12 -0700

    Alloc Memory to Array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Function to allocate memory to array
+ * @nmemb: Num of memory spaces
+ * @size: Size of nmemb
+ *
+ * Return: Pointer to allocated space or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int count = 0;
+	char *spaces;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	spaces = malloc(nmemb * size);
+
+	if (spaces == NULL)
+		return (NULL);
+	for (; count < nmemb; count++)
+		*(spaces * count) = 0;
+
+	return (spaces);
+}
