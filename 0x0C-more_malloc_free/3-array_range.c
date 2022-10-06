02f7de0a4f03dfcdbd7f759beea62743a9eca93f
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-06 14:38:38 -0700

    Create Array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * @min: Minimum starting Number
+ * @max: Maximum starting Number
+ *
+ * Return: Pointer
+ */
+int *array_range(int min, int max)
+{
+	int *n_array;
+	int diff, count = 0;
+
+	if (min > max)
+		return (NULL);
+	diff = max - min;
+
+	n_array = malloc((diff + 1) * sizeof(int));
+	if(n_array == NULL)
+		return (NULL);
+
+	for (; count <= diff; count++)
+		n_array[count] = min++;
+
+	return (n_array);
+}
