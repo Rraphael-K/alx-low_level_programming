f4eca7efe34556a314484e17efb943f60debc152
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-10 20:17:52 -0700

    Print dog details

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - Print details of dog
+ * @d: Initialize dog
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "nil";
+	if (d->age == NULL)
+		d->age = "nil";
+
+	printf("Name: %s\n Age: %.2f\n Owner: %s\n", d->name, d->age, d->owner);
+}
