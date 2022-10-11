848dac0ac0d0b812f516fc26ae003ce30ec3fe1e
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-10 20:23:05 -0700

    Print dog details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,8 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->age == NULL)
 		d->age = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
 	printf("Name: %s\n Age: %.2f\n Owner: %s\n", d->name, d->age, d->owner);
 }
