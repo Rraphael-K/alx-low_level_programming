c33eae72526bdaa81d5098fc1e14a71a73f71af3
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-10 19:33:47 -0700

    Proto

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - structure for a dog
+ * @name: Dog name
+ * @age - Dog age
+ * @owner: Dog owner
+ *
+ * Destription: Structure for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+struct dog my_dog;
+
+/endif /* _DOG_H_ */
