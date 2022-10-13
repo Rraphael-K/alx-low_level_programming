b838aa123d9e273c753f31f9b42d70d4896466f2
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-13 02:45:07 -0700

    Main to perform operation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-main.h"
+
+/**
+ * main - Perform simple operations
+ * @argc: arguments count
+ * @argv: Array of arguments
+ * Return: 0 on success, 98 on error, 99 on Illigal operator
+ */
+
+int main(int argc, char *argv[])
+{
+	/* function argument assignement */
+	int (*func)(int)(int);
+
+	/* if error, retuyrn 98 */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* get operator and assign it */
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* perform operations */
+	printf("%d\n", fucn(atoi(argv[1], atoi(argv[3]))));
+	return (0);
+}
