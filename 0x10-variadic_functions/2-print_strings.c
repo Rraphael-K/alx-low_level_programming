5243772b47eb8e392f43386833beea79f9d60460
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-13 13:41:16 -0700

    Print Strings from parameters

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (; count < n; count++)
 	{
-		string = va_arg(list char *);
+		string = va_arg(list, char *);
 		if (string == NULL)
 			printf("nil");
 		else
