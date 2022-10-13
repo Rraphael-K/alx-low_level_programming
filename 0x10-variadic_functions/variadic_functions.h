ba4a198d6516db38ae7d176186ac451fd80d6170
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-13 13:10:19 -0700

    Proto

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,5 @@
 #define _VARIADIC_FUNCTION_H
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const cahr *seperator, const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
 #endif /* _VARIADIC_FUNCTION_H */
