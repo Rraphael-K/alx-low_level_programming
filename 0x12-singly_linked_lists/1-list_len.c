53770e0f212a74136bc230219a624d4f7ae9c725
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-21 01:41:46 -0700

    Singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * list_len - length of the list
+ * @h: singly linked list
+ *
+ * Return: Num of elemnts in list
+ */
+size_t list_len(const list_t *h)
+{
+	int count;
+
+	if (h == NULL)
+		return (0);
+
+	for (count = 1; h->next != NULL; count++)
+		h = h->next;
+
+	return (count);
+}
