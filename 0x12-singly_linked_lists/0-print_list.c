4cb8b4fc70875eff6fc9beb08dbcfa902a2c22db
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-21 03:05:35 -0700

    Print all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - Func to add variables to struct list
+ * @h: Head of list
+ *
+ * Return: Num of nodes (size_t)
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	if (h == NULL)
+		return (0);
+
+	for (count = 1; h->next != NULL; count++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h->nlen, h->str);
+
+		/* assign new node */
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (count);
+}
