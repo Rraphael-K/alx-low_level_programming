f77a0c189ae14f7be2fde4cb6a4d0b5863af2b92
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-26 09:52:52 -0700

    Print number of elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *temp = h;
-	size_t count;
+	size_t count = 0;
 
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
-		count++;
 		temp = temp->next;
+		count += 1;
 	}
 	return (count);
 }
