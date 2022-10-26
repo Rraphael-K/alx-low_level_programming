0ef27ccad41a14f7aed8542e8ead7f63a40c8fef
Rraphael-K <sifisoskhoza@gmail.com>
2022-10-26 09:58:36 -0700

    Return number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print elements of linked list
+ * @h: Head of list
+ *
+ * Return: Number of nodes (count)
+ */
+
+size_t listint_len(const listint_t *h)
+{
+        const listint_t *temp = h;
+        size_t count = 0;
+
+        while (temp != NULL)
+        {
+                printf("%d\n", temp->n);
+                temp = temp->next;
+                count++;
+        }
+        return (count);
+}
