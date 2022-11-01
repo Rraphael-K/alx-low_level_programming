07489e65f012aab4c9781c70ad7b07a93bd4f0a5
Rraphael-K <sifisoskhoza@gmail.com>
2022-11-01 06:56:20 -0700

    Create new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - Create file
+ * @filename: Name of file
+ * @text_content: Content written in file
+ *
+ * Return: 1 if success or -1 if fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fb, new_l, read_w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fb = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fb == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (new_l = 0; text_content[new_l]; new_l++)
+		;
+
+	read_w = write(fb, text_content, new_l);
+	if (read_w == -1)
+		return (-1);
+
+	close(fb);
+	return (1);
+}
