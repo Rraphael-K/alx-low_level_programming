0dc6056435e4ac67817ea5d0c16b33c0599522a7
Rraphael-K <sifisoskhoza@gmail.com>
2022-11-01 06:57:51 -0700

    Create new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int fb, new_l, read_w;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fb = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fb == -1)
+	if (!fb)
 		return (-1);
 
 	if (text_content == NULL)
